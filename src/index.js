30d87a288a680d1096d86efed5bf8b4137e4b8e9
DiegoSilvadv <digssilva2000@outlook.com>
2020-11-04 22:20:44 -0300

    PWA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { registerServiceWorker } from './serviceWorker'
 
 
 ReactDOM.render(
@@ -16,4 +15,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
-registerServiceWorker();
